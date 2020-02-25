b270fe05c86a66dfa242a15a2b8f1d7ac4d619fe
Alix ANNERAUD <alix.an@outlook.fr>
2020-02-25 01:28:23 +0100

    Complete code, compilled but not tested.

--- Feedback Bridge/include/main.h
+++ Feedback Bridge/include/main.h
@@ -1,32 +1,34 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
-uint8_t Bridge_MAC_Adress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; //useless here, just for a memo
+#define NUMBER_SLAVE 2
+#define NUMBER_SLAVE_INPUTS 16
+#define UDP_PORT 8888
 
-const char * Module_1_IP_Adress = "192.168.0.101";
-const int UDP_Port;
+//Tasks Handle (not usefull here)
+xTaskHandle Scan_Slave_Handle;
+
+//Pins
+const byte S88_Clock_Pin = 2;
+const byte S88_PS_Pin = 3;
+const byte S88_Data_Out_Pin = 1;
+
+//Counters
+byte S88_Selected_Slave = 0;
+byte S88_Selected_Input = 0;
+byte S88_Clock_Counter = 0;
+
+//Modules IP 
+const char * Slave_IP_Adress[NUMBER_SLAVE] = {"192.168.0.101", "192.168.0.102"};
+//Module Input States
+unsigned int Slave_Input_Register[NUMBER_SLAVE];
+
 //WiFi network credentials
 const char * SSID = "Avrupa";
 const char * Password = "0749230994";
 
-uint8_t Ask
-
-WiFiUDP UDP;
-
-
-class Slave_Module {
-    private:
-
-    public:
-
-}
-
-
-Slave_Module Module_1;
-
-//etc ....
-
-void Tranmit();
-void Receive();
-
+//Prototypes Of Functions
+void Scan_Slave(void *pvParameters);
+void S88_PS();
+void S88_Clock();
 #endif
--- Feedback Bridge/src/main.cpp
+++ Feedback Bridge/src/main.cpp
@@ -2,15 +2,17 @@
 #include <main.h>
 #include <WiFi.h>
 
+WiFiUDP UDP;
+
 //use ESP-NOW transmition protocol
 
 void setup() {
-  Serial.begin(115200); //only for debbuging purpose
-  //Initialize ESP-NOW
+  //Setup Debug Communication
+  Serial.begin(115200);
+  //Setup Network
   Serial.print(F("Connect To "));
   Serial.print(SSID);
   Serial.print(F("Network"));
-
   WiFi.begin(SSID, Password);
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -20,18 +22,65 @@ void setup() {
   Serial.println(F("Connected !"));
   Serial.print(F("Bridge Current IP Adress :"));
   Serial.println(WiFi.localIP());
-
-  UDP.begin(UDP_Port);
+  UDP.begin(UDP_PORT);
+  //Setup S88
+  pinMode(S88_Clock_Pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(S88_Clock_Pin), S88_Clock, RISING);
+  pinMode(S88_PS_Pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(S88_PS_Pin), S88_PS, RISING);
+  pinMode(S88_Data_Out_Pin, OUTPUT);
+  //Task
+  xTaskCreatePinnedToCore(Scan_Slave, "Scan_Slave", 4096, NULL, 2, &Scan_Slave_Handle, 0);
 }
 
 void loop() {
-  uint8_t 
+  vTaskDelete(NULL);
 }
 
-void Transmit() {
-
+void Scan_Slave(void *pvParameters) {
+  (void) pvParameters;
+  uint8_t Buffer[5] = {0, 0, 0, 0, 0};
+  Serial.print(F("Start Scanning All Slave ..."));
+  for(byte i = 0; i < NUMBER_SLAVE; i++) {
+    UDP.beginPacket(Slave_IP_Adress[i], UDP_PORT);
+    UDP.write('G');
+    UDP.endPacket();
+    UDP.parsePacket();
+    byte ii = 0;
+    while(UDP.read(Buffer, 5) == 0 && ii < 50) { //wait 5000 ms slave reply - check every 100 ms
+      Serial.println(F("Waiting For Slave ..."));
+      vTaskDelay(pdMS_TO_TICKS(100));
+      ii++;
+    }
+    if(ii > 50) { //if slave doesn't reply, its ways will be protected (occupy)
+      Serial.print(F("Slave "));
+      Serial.print(i);
+      Serial.println(F("Timed Out !"));
+      Serial.println(F("Protect Slave's Railways !"));
+      Buffer[0] = 255;
+      Buffer[1] = 255;
+    }
+    else { //update local register with the buffer
+      for(byte iii = 0; iii < 8; iii++) {
+        bitWrite(Slave_Input_Register[i], iii, bitRead(Buffer[0], iii));
+      }
+      for(byte iii = 8; iii < 16; iii++) {
+        bitWrite(Slave_Input_Register[i], iii, bitRead(Buffer[1], iii));
+      }
+    }
+    vTaskDelay(10);
+  }
 }
 
-void Receive() {
+void S88_PS() {
+  S88_Selected_Slave = 0;
+  S88_Selected_Input = 0;
+}
 
+void S88_Clock() { //transmition clock for each bit
+  digitalWrite(S88_Data_Out_Pin, bitRead(Slave_Input_Register[S88_Selected_Slave], S88_Selected_Input));
+  S88_Selected_Input++;
+  if(S88_Selected_Input >= 15) {
+    S88_Selected_Slave++;
+  }
 }
